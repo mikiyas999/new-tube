e157efc51309a55721a8612c52d3a8b0286c041a
Mikiyas Getachew <mikiyasget1234@gmail.com>
2025-05-19 06:35:34 +0100

    set up clerk

--- modules/auth/ui/components/auth-button.tsx
+++ modules/auth/ui/components/auth-button.tsx
@@ -1,17 +1,50 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { UserCircleIcon } from "lucide-react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { ClapperboardIcon, UserCircleIcon, UserIcon } from "lucide-react";
 import React from "react";
 
 const AuthButton = () => {
   return (
-    <Button
-      variant="outline"
-      className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 
-          rounded-full shadow-none "
-    >
-      <UserCircleIcon />
-      Sign in
-    </Button>
+    <>
+      <SignedIn>
+        {/* <Button asChild variant="secondary"> 
+        <Link prefetch href="/studio" >
+        <ClapperboardIcon  />
+        Studio
+        </Link>
+        </Button> */}
+
+        <UserButton>
+          <UserButton.MenuItems>
+            <UserButton.Link
+              label="My profile"
+              href="/users/current"
+              labelIcon={<UserIcon className="size-4" />}
+            />
+            <UserButton.Link
+              label="Studio"
+              href="/studio"
+              labelIcon={<ClapperboardIcon className="size-4" />}
+            />
+            <UserButton.Action label="manageAccount" />
+          </UserButton.MenuItems>
+        </UserButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 
+        rounded-full shadow-none "
+          >
+            <UserCircleIcon />
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
   );
 };
 
