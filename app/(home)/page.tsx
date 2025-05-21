9bc8bf1b63cc25f8b00db46f4856dd7bd2283be6
Mikiyas Getachew <mikiyasget1234@gmail.com>
2025-05-21 19:35:55 +0100

    trpc setup

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import { HydrateClient, trpc } from "@/trpc/server";
+import { ClientGreeting } from "./client-greeting";
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Am Mike" });
   return (
-    <div>
-      <p className="text-xl p-4">The Video will be here</p>
-    </div>
+    <HydrateClient>
+      <div>...</div>
+      {/** ... */}
+      <ClientGreeting />
+    </HydrateClient>
   );
 }
