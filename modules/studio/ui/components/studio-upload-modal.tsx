3be2f00927c9a4653255b52cf470af849fa51beb
Mikiyas Getachew <mikiyasget1234@gmail.com>
2025-05-29 09:17:32 +0100

    mux integration

--- modules/studio/ui/components/studio-upload-modal.tsx
+++ modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { ResponsiveModal } from "@/components/responsive-modal";
 import { Button } from "@/components/ui/button";
 import { trpc } from "@/trpc/client";
 import { Loader2Icon, PlusIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import { StudioUploader } from "./studio-uploader";
 
 const StudioUploadModal = () => {
+  const router = useRouter();
+
   const utils = trpc.useUtils();
   const create = trpc.videos.create.useMutation({
     onSuccess: () => {
@@ -17,8 +22,25 @@ const StudioUploadModal = () => {
     },
   });
 
+  const onSuccess = () => {
+    if (!create.data?.video.id) return;
+
+    create.reset();
+    router.push(`/studio/videos/${create.data.video.id}`);
+  };
   return (
     <div>
+      <ResponsiveModal
+        title="Upload a video"
+        open={!!create.data?.url}
+        onOpenChange={() => create.reset()}
+      >
+        {create.data?.url ? (
+          <StudioUploader endpoint={create.data.url} onSuccess={onSuccess} />
+        ) : (
+          <Loader2Icon />
+        )}
+      </ResponsiveModal>
       <Button
         variant="secondary"
         onClick={() => create.mutate()}
--- modules/videos/server/procedure.ts
+++ modules/videos/server/procedure.ts
@@ -1,21 +1,43 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
+import { mux } from "@/lib/mux";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 export const videoRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const { id: userId } = ctx.user;
 
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ["public"],
+        input: [
+          {
+            generated_subtitles: [
+              {
+                language_code: "en",
+                name: "English",
+              },
+            ],
+          },
+        ],
+      },
+      cors_origin: "*",
+    });
+
     const [video] = await db
       .insert(videos)
       .values({
         userId,
         title: "Untitled",
+        muxStatus: "waiting",
+        muxUploadId: upload.id,
       })
       .returning();
 
     return {
       video: video,
+      url: upload.url,
     };
   }),
 });
