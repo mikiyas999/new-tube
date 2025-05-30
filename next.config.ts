4e1e3f689170448716208f9bafeeffad64566b0e
Mikiyas Getachew <mikiyasget1234@gmail.com>
2025-05-30 12:52:09 +0100

    config for accept image from mux

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "5mjc8h44e6.ufs.sh",
+      },
+      {
+        protocol: "https",
+        hostname: "image.mux.com",
+      },
+    ],
+  },
+  env: {
+    CLERK_SIGNING_SECRET: process.env.CLERK_SIGNING_SECRET,
+  },
 };
 
 export default nextConfig;
